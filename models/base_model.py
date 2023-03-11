88cb66a744236e9f3b2e0f1d5923e0735fa5d8c4
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-03-11 10:53:20 -0800

    created Readme and solved task 2 - 3

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""defines all common attributes/methods for other classes"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """defines all common attributes/methods for other classes"""
+
+    def __init__(self):
+        """intialises BaseModel class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def save(self):
+        """updates the public instance attribute updated_at with the
+        current datetime"""
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+
+        dict_rep = self.__dict__.copy()
+        dict_rep['created_at'] = self.created_at.isoformat()
+        dict_rep['id'] = self.id
+        dict_rep['updated_at'] = self.updated_at.isoformat()
+        dict_rep['__class__'] = self.__class__.__name__
+        return dict_rep
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        className = self.__class__.__name__
+        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
