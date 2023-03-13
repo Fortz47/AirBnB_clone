bc26ff25d6367d659730eb4a024186242ee47e7b
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-03-13 09:47:50 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,8 @@ class BaseModel:
             if k is 'created_at' or k is 'updated_at':
                 self.__dict__[k] = datetime.strptime(v, tform)
             else self.__dict__[k] = v
+            if key not in ['__class__']:
+                    setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
